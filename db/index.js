c32bbec2a7fee45805eb42d4a878d178c5e3e73d
Muhaddis Irshad <muhaddisirshad58@gmail.com>
2026-03-24 05:09:03 +0500

    Change the DB password for security reasons

--- db/index.js
+++ db/index.js
@@ -5,8 +5,9 @@ dotenv.config();
 // console.log(`DB NAME = ${process.env.DB_NAME}`)
 // console.log(`DB PASSWORD = ${process.env.DB_PASSWORD}`)
 // console.log(`DB USER = ${process.env.DB_USER}`)
+const password = encodeURIComponent(process.env.DB_PASSWORD);
 
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@practice-cluster.clujnmz.mongodb.net/?appName=${process.env.DB_NAME}`;
+const url = `mongodb+srv://${process.env.DB_USER}:${password}@practice-cluster.clujnmz.mongodb.net/?appName=${process.env.DB_NAME}`;
 
 mongoose.connect(url);
 
